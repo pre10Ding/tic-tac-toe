e35956bdd2f2ec5b385d2d90128acdcd70cd00a8
Cheng Shao <cheng.shao38@gmail.com>
2021-01-25 15:46:03 -0500

    Fix loop logic and add some comments.

--- main.rb
+++ main.rb
@@ -1,12 +1,13 @@
-require "./interface.rb"
-require "./player.rb"
+require './interface'
+require './player'
 
 player1 = Player.new(0)
 player2 = Player.new(1)
 ui = Interface.new(player1,player2)
 
-while(true) do #call ui method 
-  unless ui.do_turn #if do_turn returns false, game needs to be reset
+ui.display(ui.players)
+while (true) do # call ui method 
+  unless ui.do_turn # if do_turn returns false, game needs to be reset
     player1 = Player.new(0)
     player2 = Player.new(1)
     ui = Interface.new(player1,player2)
